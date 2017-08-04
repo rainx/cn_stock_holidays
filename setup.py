666aad779f50f58d352e4fb3681dccc2662e57f2
RainX <i@rainx.cc>
2017-08-04 13:48:57 +0800

    add function cache

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cn-stock-holidays",
-    version = "0.9",
+    version = "0.10",
     packages = find_packages(),
     install_requires=[
         'requests'
