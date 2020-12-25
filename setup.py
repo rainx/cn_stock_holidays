d2e2bb21f84b3a97bdff0b8c74c19f770dd659e5
RainX <i@rainx.cc>
2020-12-25 16:32:58 +0800

    更新了2021年国内股市节假日

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="cn-stock-holidays",
-    version="1.7",
+    version="1.8",
     packages=find_packages(),
     install_requires=[
         'requests'
@@ -12,6 +17,8 @@ setup(
     author="rainx",
     author_email="i@rainx.cn",
     description="A List of china stock exchange holidays",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
     keywords="china stock holiday exchange shanghai, shenzhen and hongkong",
     url="https://github.com/rainx/cn_stock_holidays.git",  # project home page, if any
